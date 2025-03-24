e8e2636204ab17bd4b3d19677fec2e74bbcf25c8
Gian Martín Tituaña Intriago <mtituanai@estud.usfq.edu.ec>
2025-03-24 15:36:59 -0500

    Add files via upload

--- src/model/Profesor.java
+++ src/model/Profesor.java
@@ -1,17 +1,19 @@
 package com.ejemplo.coilmatching.model;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
-public class Curso {
+public class Profesor {
     @Id
-    private String codigo;
+    private String email;
     private String nombre;
-    private String idioma;
-    private String tematica;
 
     @ManyToOne
     private Universidad universidad;
+
+    @ManyToMany
+    private List<Curso> cursos;
 
     // Getters y Setters
 }
